8016b1724b5fe39ef4e5018ad2a97f3fa0bb10fc
Rytis Cepulis <cepRytis@gmail.com>
2022-05-12 18:20:19 +0300

    fixed for py file

--- Python_LW2/Python_LW2.py
+++ Python_LW2/Python_LW2.py
@@ -1,18 +1,27 @@
 import pymongo
 from pymongo import MongoClient
 import json
+import sys
+import os
 
-DATA_JSON_PATH = 'retaurants.json'
+DATA_JSON_PATH = os.path.join(sys.path[0], 'retaurants.json')
 
-cluster = MongoClient('mongodb+srv://sa:sa@cluster0.n9p5p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-db = cluster["lw2"]
-collection = db["lw2"]
+def loadDataIntoDB(collection):
+    data = []
+    with open(DATA_JSON_PATH) as f:
+        for line in f:
+            data.append(json.loads(line))
 
-data = []
-with open(DATA_JSON_PATH) as f:
-    for line in f:
-        data.append(json.loads(line))
+    data
+    collection.insert_many(data)
 
-data
+def getDbCollection():
+    cluster = MongoClient('mongodb+srv://sa:sa@cluster0.n9p5p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    db = cluster["lw2"]
+    return db["lw2"]
 
-collection.insert_many(data)
+
+collection = getDbCollection()
+loadDataIntoDB(collection)
+print("yes")
+
