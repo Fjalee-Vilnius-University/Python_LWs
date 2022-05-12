3637a353b15d98347373cdb004647c9688cfb351
Rytis Cepulis <cepRytis@gmail.com>
2022-05-12 18:44:54 +0300

    task3

--- Python_LW2/Python_LW2.py
+++ Python_LW2/Python_LW2.py
@@ -24,6 +24,11 @@ def print_query(query):
     for line in query:
         print(line)
 
+def print_one_from_query(query):
+    for line in query:
+        print(line)
+        break
+
 def query_all(collection):
     query = {
         "restaurant_id": {
@@ -32,17 +37,32 @@ def query_all(collection):
     }
     return collection.find(query)
 
+def query_task3(collection):
+    q_from = {
+        "restaurant_id": {
+            "$regex": '.*?',
+        }
+    }
+    q_select = {
+        "restaurant_id": 1,
+        "name": 1,
+        "borough": 1,
+        "cuisine": 1
+    }
+    return collection.find(q_from, q_select)
+
 collection = get_db_collection()
+
 # load_data_into_db(collection)
 
 print_query(query_all(collection))
-print_query(query_all(collection))
+print_query(query_task3(collection))
 
 
-#       Sukurkite restoranų duomenų rinkinį (pridedamas zip failas)
-#       Parašykite užklausą atvaizduojančią visus dokumentus iš restoranų rinkinio
-# Parašykite užklausą, kuri atvaizduotų laukus - restaurant_id, name, borough ir cuisine - visiems dokumentams
-# Parašykite užklausą, kuri atvaizduotų laukus - restaurant_id, name, borough ir cuisine -, bet nerodytų lauko field_id visiems dokumentams
-# Parašykite užklausą, kuri parodytų visus miestelio Bronx restoranus
-# Parašykite užklausą, kuri parodytų restoranus su įvertinimu tarp 80 ir 100.
-# Parašykite užklausą, kad cuisine būtų išdėstyta didėjimo tvarka, o borough - mažėjimo.
+#       1.Sukurkite restoranų duomenų rinkinį (pridedamas zip failas)
+#       2.Parašykite užklausą atvaizduojančią visus dokumentus iš restoranų rinkinio
+#       3.Parašykite užklausą, kuri atvaizduotų laukus - restaurant_id, name, borough ir cuisine - visiems dokumentams
+# 4.Parašykite užklausą, kuri atvaizduotų laukus - restaurant_id, name, borough ir cuisine -, bet nerodytų lauko field_id visiems dokumentams
+# 5.Parašykite užklausą, kuri parodytų visus miestelio Bronx restoranus
+# 6.Parašykite užklausą, kuri parodytų restoranus su įvertinimu tarp 80 ir 100.
+# 7.Parašykite užklausą, kad cuisine būtų išdėstyta didėjimo tvarka, o borough - mažėjimo.
