14511f8210352798f689f1f102ec8896683fa548
Rytis Cepulis <cepRytis@gmail.com>
2022-05-12 18:49:12 +0300

    task5

--- Python_LW2/Python_LW2.py
+++ Python_LW2/Python_LW2.py
@@ -66,6 +66,19 @@ def query_task4(collection):
     }
     return collection.find(q_from, q_select)
 
+def query_task5(collection):
+    q_from = {
+        "borough": "Bronx"
+    }
+    q_select = {
+        "restaurant_id": 1,
+        "name": 1,
+        "borough": 1,
+        "cuisine": 1,
+        "_id": 0
+    }
+    return collection.find(q_from, q_select)
+
 collection = get_db_collection()
 
 # load_data_into_db(collection)
@@ -73,12 +86,14 @@ collection = get_db_collection()
 # print_one_from_query(query_all(collection))
 # print_one_from_query(query_task3(collection))
 # print_one_from_query(query_task4(collection))
+print_one_from_query(query_task5(collection))
+
 
 
 #       1.Sukurkite restoranų duomenų rinkinį (pridedamas zip failas)
 #       2.Parašykite užklausą atvaizduojančią visus dokumentus iš restoranų rinkinio
 #       3.Parašykite užklausą, kuri atvaizduotų laukus - restaurant_id, name, borough ir cuisine - visiems dokumentams
-# 4.Parašykite užklausą, kuri atvaizduotų laukus - restaurant_id, name, borough ir cuisine -, bet nerodytų lauko field_id visiems dokumentams
+#       4.Parašykite užklausą, kuri atvaizduotų laukus - restaurant_id, name, borough ir cuisine -, bet nerodytų lauko field_id visiems dokumentams
 # 5.Parašykite užklausą, kuri parodytų visus miestelio Bronx restoranus
 # 6.Parašykite užklausą, kuri parodytų restoranus su įvertinimu tarp 80 ir 100.
 # 7.Parašykite užklausą, kad cuisine būtų išdėstyta didėjimo tvarka, o borough - mažėjimo.
