b4a91fa0c156e76c1a62263da711dfa40bd5ae66
Rytis Cepulis <69936179+Fjalee@users.noreply.github.com>
2022-05-15 16:29:10 +0300

    Lw4 rewrite (#3)
    * wrong import
    
    * added example
    
    * remade into my function
    
    * error fixes
    
    * added fibonacci function
    
    * renaming to the right name
    
    * building module
    
    * added my structure
    
    * added needed methods
    
    * added function to change triangle

---
+++ Python_LW4/Extension/main.py
@@ -0,0 +1,4 @@
+import fibonacciNthNm
+
+result = fibonacciNthNm.fibonacci_nth_nm(10)
+print(result)
--- Python_LW4/myModule/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, Extension
-
-module = Extension("myModule", sources = ["main.c"])
-
-setup(name="PackageName",
-    version = "1.0",
-    description = "my C module",
-    ext_modules = [module])
